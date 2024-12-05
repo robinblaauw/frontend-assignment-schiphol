91ab8cf54d7b8e637e17671297d048c309949988
Robin Blaauw <robin.blaauw@persgroep.net>
2024-12-05 12:37:55 +0100

    added sorting to flights

---
+++ src/constants/flightFilters.ts
@@ -0,0 +1,4 @@
+export enum FilterOptions {
+  EARLIEST_TO_LATEST = "Earliest to Latest",
+  LATEST_TO_EARLIEST = "Latest to Earliest",
+}
--- src/util/filterFlights.ts
+++ src/util/filterFlights.ts
@@ -1,13 +1,33 @@
+import { FilterOptions } from "@/constants/flightFilters";
 import { FlightType } from "@/types/flights";
 
+const sortFlights = (flights: FlightType[], sortBy: FilterOptions) => {
+  if (sortBy === FilterOptions.EARLIEST_TO_LATEST) {
+    return flights.sort((a, b) => {
+      const dateTimeA = new Date(`${a.date}T${a.expectedTime}`);
+      const dateTimeB = new Date(`${b.date}T${b.expectedTime}`);
+      return dateTimeA.getTime() - dateTimeB.getTime();
+    });
+  } else if (sortBy === FilterOptions.LATEST_TO_EARLIEST) {
+    return flights.sort((a, b) => {
+      const dateTimeA = new Date(`${a.date}T${a.expectedTime}`);
+      const dateTimeB = new Date(`${b.date}T${b.expectedTime}`);
+      return dateTimeB.getTime() - dateTimeA.getTime();
+    });
+  }
+
+  return flights;
+};
 export const filterFlights = (
   flights: FlightType[],
-  filter: string
+  filter: string,
+  sortBy: FilterOptions
 ): FlightType[] => {
-  if (!filter) return flights;
+  if (!filter || filter.length < 3) return sortFlights(flights, sortBy);
 
+  console.log(sortBy);
   const lowerCaseFilter = filter.toLowerCase();
-  return flights
+  return sortFlights(flights, sortBy)
     .filter((flight) =>
       Object.values(flight).some((value) =>
         value.toLowerCase().includes(lowerCaseFilter)
@@ -19,12 +39,13 @@ export const filterFlights = (
 export const filterFlightsByKey = (
   flights: FlightType[],
   filter: string,
-  key: keyof FlightType
+  key: keyof FlightType,
+  sortBy: FilterOptions
 ): FlightType[] => {
-  if (!filter) return flights;
+  if (!filter || filter.length < 3) return sortFlights(flights, sortBy);
 
   const lowerCaseFilter = filter.toLowerCase();
-  return flights
+  return sortFlights(flights, sortBy)
     .filter((flight) => {
       return flight[key].toLowerCase().includes(lowerCaseFilter);
     })
